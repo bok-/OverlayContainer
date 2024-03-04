d77af0812beeef9f7fd15276e79334756a15ebbf
Rob Amos <bok-@users.noreply.github.com>
2024-03-04 18:29:01 +1100

    Get rid of shitty dependencies

--- Package.swift
+++ Package.swift
@@ -13,8 +13,6 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
-        .package(url: "https://github.com/Quick/Quick", from: "3.1.0"),
     ],
     targets: [
         .target(
@@ -24,8 +22,6 @@ let package = Package(
             name: "OverlayContainerTests",
             dependencies: [
                 "OverlayContainer",
-                "Nimble",
-                "Quick"
             ]
         )
     ],
